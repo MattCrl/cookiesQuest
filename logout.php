aa8cad85d95f33c4f115ed3e45442e1d6023d268
Matt Crl <matth.carlevaris@gmail.com>
2018-04-15 10:52:35 +0200

    remove cookies when logout

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
 <?php
 
 session_start();
+setcookie('cookieadded[46]', "", time()-3600);
+setcookie('cookieadded[36]', "", time()-3600);
+setcookie('cookieadded[58]', "", time()-3600);
+setcookie('cookieadded[32]', "", time()-3600);
 session_destroy();
 header('Location: index.php');
